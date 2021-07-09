7f47192ceb49abef9479fe1d680b0732074cfd38
sina <SINA@GMAIL.COM>
2021-07-09 19:50:20 +0430

    add users and roles and jwt

---
+++ src/main/java/com/sina/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.sina.repository;
+
+import com.sina.entity.Role;
+import com.sina.enums.RoleEnum;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleEnum name);
+}
